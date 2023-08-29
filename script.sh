f6c1a91580778d275d8b4a1b01d916642a921fa5
Sahil Patel <ssahillppatell@gmail.com>
2023-08-29 22:59:19 +0530

    Added azure cli integration

--- script.sh
+++ script.sh
@@ -1,6 +1,24 @@
 #! bin/bash
 
-# This script decrypts the dbeaver credentials file and then updates it with the new password and re-encrypts it.
+# Install azure cli for any OS
+if [[ $OSTYPE == "darwin"* ]]; then
+    if [[ $(brew --version) == "" ]]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew update && brew install azure-cli
+else
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+fi
+
+# Login to azure only if not logged in
+if [[ $(az account show) == "" ]]; then
+    az login --allow-no-subscriptions
+fi
+
+# Get access token for oss-rdbms and extract the access token without jq
+access_token=$(az account get-access-token --resource-type oss-rdbms | grep accessToken | cut -d '"' -f 4)
+
+# ------------------------------------------------------------------------------------------------------------
 
 path_to_dbeaver_credentials_file="$HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json"
 
@@ -15,36 +33,23 @@ initialization_vector=00000000000000000000000000000000
 # Decrypt the file using openssl
 decrypted_file=$(openssl aes-128-cbc -d -K $pass_key -iv $initialization_vector -in $path_to_dbeaver_credentials_file | dd bs=1 skip=16 2>/dev/null)
 
-new_password=123567
+# Username for which the password needs to be updated comes from the command line
+username=$1
+new_password=$access_token
 
-# Sample output of decrypted file: {"postgres-jdbc-18a4199bb55-600e2ae54ff4ee5a":{"#connection":{"user":"postgres","password":"123567"}}}
-# Update the password of connection with user as postgres
-
-updated_file=$(echo $decrypted_file | sed -E "s/(\"postgres-jdbc-[a-z0-9]+-[a-z0-9]+\"):(\{\"#connection\":\{\"user\":\"postgres\",\"password\":\")([a-z0-9]+)(\"}}\})/\1:\2$new_password\4/g")
+# Update the password of connection with username
+updated_file=$(echo $decrypted_file | sed -E "s/(\"postgres-jdbc-[a-z0-9]+-[a-z0-9]+\"):(\{\"#connection\":\{\"user\":\"$username\",\"password\":\")([a-z0-9]+)(\"}}\})/\1:\2$new_password\4/g")
 
 # Add 16 bytes of initialization vector to the updated file
 _16_bytes_of_initialization_vector=$(echo -n $initialization_vector | dd bs=1 count=16 2>/dev/null)
-echo $_16_bytes_of_initialization_vector
 updated_file=$(echo -n $_16_bytes_of_initialization_vector$updated_file)
 
-echo $updated_file
-
 # Encrypt the file using with the required skip
-encrypted_file=$(openssl enc -aes-128-cbc -K $pass_key -iv $initialization_vector -in <(echo -n $updated_file) -out credentials-config.json | dd bs=1 seek=16 2>/dev/null)
-
-xxd credentials-config.json
-
-# decrypt the local file
-echo $(openssl aes-128-cbc -d -K $pass_key -iv $initialization_vector -in credentials-config.json | dd bs=1 skip=16 2>/dev/null)
-
-# remove the unencrypted file
-# rm credentials-config.json
+encrypted_file=$(openssl enc -aes-128-cbc -K $pass_key -iv $initialization_vector -in <(echo -n $updated_file) -out temp-credentials-config.json | dd bs=1 seek=16 2>/dev/null)
 
 # move the encrypted file to the required location
-mv credentials-config.json $path_to_dbeaver_credentials_file
+mv temp-credentials-config.json $path_to_dbeaver_credentials_file
 
-
-
-
-
+# remove the unencrypted file
+rm temp-credentials-config.json
 
